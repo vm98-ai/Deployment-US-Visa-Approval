68a46884551e01b09814ba4b3a45230137abbe2b
vishal98m <vishal.98m@gmail.com>
2025-03-15 22:32:30 +0530

    Message

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "us_visa",
+    version = "0.0.0",
+    author="vm",
+    author_email= "vm@gmail.com", #set your own
+    packages = find_packages())
