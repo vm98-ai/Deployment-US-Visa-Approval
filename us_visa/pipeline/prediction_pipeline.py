d123aec41b41612307c8b85aeeb1ae515e0541e7
vishal98m <vishal.98m@gmail.com>
2025-04-07 02:16:11 +0530

    Message

--- us_visa/pipeline/prediction_pipeline.py
+++ us_visa/pipeline/prediction_pipeline.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 import numpy as np
 import pandas as pd
 from us_visa.entity.config_entity import USvisaPredictorConfig
@@ -7,14 +8,26 @@ from us_visa.entity.s3_estimator import UsvisaEstimator
 from us_visa.exception import USvisaException
 from us_visa.logger import logging
 from us_visa.utils.main_utils import read_yaml_file
+from pandas import DataFrame
 
 
 class USvisaData:
-    def __init__(self, continent, education_of_employee, has_job_experience,
-                 requires_job_training, no_of_employees, region_of_employment,
-                 prevailing_wage, unit_of_wage, full_time_position,
-                 company_age):
-        
+    def __init__(self,
+                continent,
+                education_of_employee,
+                has_job_experience,
+                requires_job_training,
+                no_of_employees,
+                region_of_employment,
+                prevailing_wage,
+                unit_of_wage,
+                full_time_position,
+                company_age
+                ):
+        """
+        Usvisa Data constructor
+        Input: all features of the trained model for prediction
+        """
         try:
             self.continent = continent
             self.education_of_employee = education_of_employee
@@ -22,48 +35,62 @@ class USvisaData:
             self.requires_job_training = requires_job_training
             self.no_of_employees = no_of_employees
             self.region_of_employment = region_of_employment
-            self.prevailing_wage=prevailing_wage
-            self.unit_of_wage=unit_of_wage
-            self.full_time_position=full_time_position
-            self.company_age=company_age
+            self.prevailing_wage = prevailing_wage
+            self.unit_of_wage = unit_of_wage
+            self.full_time_position = full_time_position
+            self.company_age = company_age
+
 
         except Exception as e:
-            raise USvisaException(e,sys) from e
+            raise USvisaException(e, sys) from e
 
-    def get_usvisa_input_data_frame(self) -> pd.DataFrame:
-
+    def get_usvisa_input_data_frame(self)-> DataFrame:
+        """
+        This function returns a DataFrame from USvisaData class input
+        """
         try:
+            
             usvisa_input_dict = self.get_usvisa_data_as_dict()
-            return pd.DataFrame(usvisa_input_dict)
+            return DataFrame(usvisa_input_dict)
+        
         except Exception as e:
-            raise USvisaException(e,sys) from e
+            raise USvisaException(e, sys) from e
+
 
     def get_usvisa_data_as_dict(self):
+        """
+        This function returns a dictionary from USvisaData class input 
+        """
+        logging.info("Entered get_usvisa_data_as_dict method as USvisaData class")
 
-        logging.info("Entered get usvisa data as dict function of prediction pipeline")
         try:
             input_data = {
-            "continent" : [self.continent],
-            "education_of_employee" : [self.education_of_employee],
-            "has_job_experience" : [self.has_job_experience],
-            "requires_job_training" : [self.requires_job_training],
-            "no_of_employees" : [self.no_of_employees],
-            "region_of_employment" : [self.region_of_employment],
-            "prevailing_wage": [self.prevailing_wage],
-            "unit_of_wage": [self.unit_of_wage],
-            "full_time_position": [self.full_time_position],
-            "company_age": [self.company_age],
+                "continent": [self.continent],
+                "education_of_employee": [self.education_of_employee],
+                "has_job_experience": [self.has_job_experience],
+                "requires_job_training": [self.requires_job_training],
+                "no_of_employees": [self.no_of_employees],
+                "region_of_employment": [self.region_of_employment],
+                "prevailing_wage": [self.prevailing_wage],
+                "unit_of_wage": [self.unit_of_wage],
+                "full_time_position": [self.full_time_position],
+                "company_age": [self.company_age],
             }
 
-            logging.info("Entered the in the correct format")
+            logging.info("Created usvisa data dict")
+
+            logging.info("Exited get_usvisa_data_as_dict method as USvisaData class")
+
+            return input_data
 
         except Exception as e:
-            raise USvisaException(e,sys)
-
+            raise USvisaException(e, sys) from e
 
 class USvisaClassifier:
     def __init__(self,prediction_pipeline_config: USvisaPredictorConfig = USvisaPredictorConfig(),) -> None:
-        
+        """
+        :param prediction_pipeline_config: Configuration for prediction the value
+        """
         try:
             # self.schema_config = read_yaml_file(SCHEMA_FILE_PATH)
             self.prediction_pipeline_config = prediction_pipeline_config
@@ -72,7 +99,10 @@ class USvisaClassifier:
 
 
     def predict(self, dataframe) -> str:
-       
+        """
+        This is the method of USvisaClassifier
+        Returns: Prediction in string format
+        """
         try:
             logging.info("Entered predict method of USvisaClassifier class")
             model = UsvisaEstimator(
